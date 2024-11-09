a640860218200143768e54b9b3ae6e859163ab85
negeek7 <negeek7@gmail.com>
2024-11-09 17:41:01 +0530

    floater working

--- entrypoints/floater.js
+++ entrypoints/floater.js
@@ -1,15 +1,16 @@
-const floater = document.createElement('div');
+import {defineUnlistedScript} from "wxt/sandbox";
 
+export default defineUnlistedScript(() => {
+const floater = document.createElement('div');
 
 floater.id = "testFloater";
 
-
-floater.style.position = "fixed";
+floater.style.position = 'fixed';
 floater.style.bottom = '20px';
 floater.style.right = '20px';
 floater.style.width = '150px';
 floater.style.height = '50px';
-floater.style.backgroundColor = '#4CAF50';
+floater.style.backgroundColor = 'black';
 floater.style.color = '#ffffff';
 floater.style.display = 'flex';
 floater.style.alignItems = 'center';
@@ -17,7 +18,15 @@ floater.style.justifyContent = 'center';
 floater.style.borderRadius = '8px';
 floater.style.cursor = 'pointer';
 floater.style.zIndex = '1000';
-floater.innerText = 'Click me!';
+
+floater.textContent = 'Click me!';
+
+floater.addEventListener('click', () => {
+    console.log("FLOATER CLICKED!")
+});
 
 
 document.body.appendChild(floater);
+})
+
+
--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,21 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    action: {
+      default_title: 'Helllo Title',
+    },
+    web_accessible_resources: [
+      {
+        matches: ['*://*.google.com/*'],
+        resources: ['icon/*.png'],
+      },
+    ],
+    content_scripts: [
+      {
+        matches: ["*://*/*"],
+        js: ["floater.js"]
+      }
+    ]
+  },
 });
